e61e2bd87bfddeef4afdf0dcbdd80dad646541a6
keefeharris <keefeharrisjr@gmail.com>
2021-07-04 20:48:21 -0400

    Update index.js
    if applied, morgan was applied while example code was adjusted

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
 //the module for express is called
 const app = express();
 //the module express is encapsulated 
+
 
 let myLogger = (req, res, next) => {
   console.log(req.url);
@@ -13,24 +15,24 @@ let requestTime = (req, res, next) => {
   req.requestTime = Date.now();
   next();
 };
-//this is a middleware function that logs a time upon req
+//this is a middleware function that logs a time upon req (timestamp)
 
 app.use(myLogger);
 app.use(requestTime);
 //invoking the app.use on the middleware function ensures that these middleware functions will be called upon every request
-
+app.use(morgan('common'));
+/*
+Morgan’s “common” format, which logs basic data such as IP address, the time of the request, 
+the request method and path, as well as the status code that was sent back as a response.
+"check terminal for date, time, etc....." 
+*/
 
 app.get('/', (req, res) => {
-  let responseText = 'Welcome to my app!';
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
-});
-
+    res.send('Welcome to my app!');
+  });
+  
 app.get('/secreturl', (req, res) => {
-  let responseText = 'This is a secret url with super top-secret content.<br>';
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
-
+    res.send('This is a secret url with super top-secret content.');
 });
 
 app.listen(8080, () => {
