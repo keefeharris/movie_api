94d6da1a5616f803eae68a1b1c70db0e56041e17
keefeharris <keefeharrisjr@gmail.com>
2021-07-11 16:03:17 -0400

    Update mongoose.js
    if applied, mongoose npm was loaded, we connected to mongo database and made our user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,2 +1,45 @@
+/*
+    CONNECT TO DATABASE
+*/
 const mongoose = require('mongoose')
 //mongoose variable is used to load mongoose npm
+
+//provides a url and options object
+mongoose.connect('mongodb://127.0.0.1:27017/movie-api',  {
+    useNewUrlParser: true,
+    useCreateIndex: true
+    //this parses the mongodb string
+    //make sure that when mongoose is works with mongoDB, indeexes are created allowing us to access the data we need to access
+})
+
+/*
+    CREATE A MODEL
+*/
+
+const User = mongoose.model('User', {
+    userName: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    favorites: {
+
+    }
+})
+//2 arguments are the name of the model and the name of the fields that we want
+
+
+
+
+
+
+
+
+
+
+
+
