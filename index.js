23e06ab6b0443ea2fb7129916c0c0ca6ce009937
keefeharris <keefeharrisjr@gmail.com>
2021-07-27 09:55:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 const express = require('express');
 //the module for express is called
 const app = express();
 //the module express is encapsulated 
 const morgan = require('morgan');
 //the module for morgan is called
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 //the module for body-parser is called
-//const uuid = require('uuid');
+const uuid = require('uuid');
 //the module for uuid is called
-
-
 
 let myLogger = (req, res, next) => {
   console.log(req.url);
@@ -62,7 +30,7 @@ Morgan’s “common” format, which logs basic data such as IP address, the ti
 the request method and path, as well as the status code that was sent back as a response.
 "check terminal for date, time, etc....." 
 */
-/*
+
 app.use(express.static('public'));
 //This function automatically routes all requests for static files to their corresponding files within a certain folder on the server
 
@@ -71,7 +39,16 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 //This is an error-handling middleware function that will log all application-level errors to the terminal.
+app.use(express.json());
 
+function capitalizeTheFirstLetterOfEachWord(words) {
+    var separateWord = words.toLowerCase().split(' ');
+    for (var i = 0; i < separateWord.length; i++) {
+       separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+       separateWord[i].substring(1);
+    }
+    return separateWord.join(' ');
+}
 
 let movies = [
     {
@@ -126,7 +103,29 @@ let movies = [
     }
 ];
 
-/*
+let genres = [
+    {
+        name: "Action",
+        description: "Action film is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, rescues and frantic chases."
+    },
+    {
+        name: "Crime",
+        description: "Crime fiction is the literary genre that fictionalises crimes, their detection, criminals and their motives"
+    },
+    {
+        name: "Drama",
+        description: "The drama genre is strongly based in a character, or characters, that are in conflict at a crucial moment in their lives. Most dramas revolve around families and often have tragic or painful resolutions."
+    },
+    {
+        name: "Adventure",
+        description: " An adventure is an event or series of events that happens outside the course of the protagonists ordinary life, usually accompanied by danger, often by physical action."
+    },
+    {
+        name: "Western",
+        description: "Western is a genre of fiction set primarily in the latter half of the 19th and early 20th century in the Western United States, which is styled the 'Old West'."
+    }
+]
+
 let directors = [
     {
         name: "Frank Darabont",
@@ -177,9 +176,12 @@ let directors = [
         movie: ""
     }
 ];
-*/
-/*
-//Return a greeting message
+
+let users = [
+
+];
+
+//Return a greeting message to test server
 app.get('/', (req, res) => {
   res.send('Welcome to Movie REST API!');
 });
@@ -189,22 +191,54 @@ app.get('/movies', (req, res) => {
   res.json(movies);
 });
 
-//Return movie about a single movie by title to the user
+//Return a single movie by title to the user
 app.get('/movies/:title', (req, res) => {
     const _title = req.params.title
-
-    movie.findById(_title).then((movie) => {
-        if(!movie) {
-            return res.status(404).send()
-        }
-        res.send(movie)
-    }).catch((e) => {
-        res.status(500).send()
+    movies.forEach((movie) => {
+        if (movie.title === capitalizeTheFirstLetterOfEachWord(_title)) {
+            res.json(movie);
+        } 
     })
-})
+    return res.status(404).send("Movie not found.");
+});
+
+//return a single genre by name to user
+app.get('/genres/:name', (req, res) => {
+    const _gname = req.params.name
+    genres.forEach((genre_name) => {
+        if (genre_name.name === capitalizeTheFirstLetterOfEachWord(_gname)) {
+            res.json(genre_name);
+        }
+    })
+    return res.status(404).send("Genre not found.");
+});
+
+//Return a single director by name to user
+app.get('/directors/:name', (req, res) => {
+    const _name = req.params.name
+    directors.forEach((director) => {
+        if (director.name === capitalizeTheFirstLetterOfEachWord(_name)) {
+            res.json(director);
+        }
+    })
+    return res.status(404).send("Director not found.");
+});
+
+//Allow new users to register
+app.post('/users', (req, res) => {
+    let newUser = req.body;
+
+    if (!newUser.name) {
+        const message = 'The username is missing.';
+        res.status(400).send(message);
+    } else {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }
+});
 
 //Open port 8080 which enables us to send and recieve through the server
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
-*/
