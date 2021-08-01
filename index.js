a7f2423b23d79c9cf2713f1616bac5db9af9667a
keefeharris <keefeharrisjr@gmail.com>
2021-08-01 10:16:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+const mongoose = require('mongoose');
+//requires the mongoose package
+const Models = require('./models.js');
+//requires the mongoose models in models.js
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+//allows mongoose to connect to database to perform CRUD operations
+
 const express = require('express');
 //the module for express is called
 const app = express();
 //the module express is encapsulated 
+
+const bodyParser = require('body-parser');
+//the module for body-parser is called
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const morgan = require('morgan');
 //the module for morgan is called
 const uuid = require('uuid');
 //the module for uuid is called
-const bodyParser = require('body-parser');
-//the module for body-parser is called
+
 
 
 let myLogger = (req, res, next) => {
@@ -40,7 +58,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 //This is an error-handling middleware function that will log all application-level errors to the terminal.
-app.use(bodyParser.json());
+
 
 function capitalizeTheFirstLetterOfEachWord(words) {
     var separateWord = words.toLowerCase().split(' ');
@@ -51,154 +69,6 @@ function capitalizeTheFirstLetterOfEachWord(words) {
     return separateWord.join(' ');
 }
 
-let movies = [
-    {
-        title: "The Shawshank Redemption",
-        genre: "drama",
-        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
-    },
-    {
-        title: "The Godfather",
-        genre: "crime",
-        description: "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son."
-    },
-    {
-        title: "The Godfather: Part 2",
-        genre: "crime",
-        description: "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate."
-    },
-    {
-        title: "The Dark Knight",
-        genre: "action",
-        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
-    },
-    {
-        title: "Angry Men",
-        genre: "crime",
-        description: "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence."
-    },
-    {
-        title: "Schindler's List",
-        genre: "biography",
-        description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis"
-    },
-    {
-        title: "The Lord Of The Rings: The Return Of The King",
-        genre: "adventure",
-        description: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."
-    },
-    {
-        title: "PulpFiction",
-        genre: "Drama",
-        description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
-    },
-    {
-        title: "The Good, The Bad And The Ugly",
-        genre: "western",
-        description: "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery."
-    },
-    {
-        title: "The Lord Of The Rings: The Fellowship Of The Rings",
-        genre: "adventure",
-        description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron"
-    }
-];
-
-let genres = [
-    {
-        name: "Action",
-        description: "Action film is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, rescues and frantic chases."
-    },
-    {
-        name: "Crime",
-        description: "Crime fiction is the literary genre that fictionalises crimes, their detection, criminals and their motives"
-    },
-    {
-        name: "Drama",
-        description: "The drama genre is strongly based in a character, or characters, that are in conflict at a crucial moment in their lives. Most dramas revolve around families and often have tragic or painful resolutions."
-    },
-    {
-        name: "Adventure",
-        description: " An adventure is an event or series of events that happens outside the course of the protagonists ordinary life, usually accompanied by danger, often by physical action."
-    },
-    {
-        name: "Western",
-        description: "Western is a genre of fiction set primarily in the latter half of the 19th and early 20th century in the Western United States, which is styled the 'Old West'."
-    }
-]
-
-let directors = [
-    {
-        name: "Frank Darabont",
-        birthYear: "January 28, 1959",
-        bio: "Frank Árpád Darabont is a French-American film director, screenwriter and producer of Hungarian descent. He has been nominated for three Academy Awards and a Golden Globe Award.",
-        movie: ""
-    },
-    {
-        name: "Francis Ford Coppola",
-        birthYear: "April 7, 1939",
-        bio: "Francis Ford Coppola was born in 1939 in Detroit, Michigan, but grew up in a New York suburb in a creative, supportive Italian-American family.",
-        movie: ""
-    },
-    {
-        name: "Christopher Nolan",
-        birthYear: "July 30, 1970",
-        bio: "Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
-        movie: ""
-    },
-    {
-        name: "Sidney Lumet",
-        birthYear: " June 25, 1924",
-        bio: "Sidney Arthur Lumet was an American film director, producer, and screenwriter with over 50 films to his credit.",
-        movie: ""
-    },
-    {
-        name: "Steven Spielberg",
-        birthYear: "December 18, 1946",
-        bio: "One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood's best known director and one of the wealthiest filmmakers in the world.",
-        movie: ""
-    },
-    {
-        name: "Peter Jackson",
-        birthYear: " October 31, 1961",
-        bio: "Sir Peter Robert Jackson ONZ KNZM is a New Zealand film director, producer, and screenwriter. He is best known as the director, writer, and producer.",
-        movie: ""
-    },
-    {
-        name: "Quentin Tarantino",
-        birthYear: " March 27, 1963",
-        bio: "Quentin Jerome Tarantino is an American film director, screenwriter, producer, author, and actor. His films are characterized by nonlinear storylines, dark humor, stylized violence, extended dialogue, ensemble casts, references to popular culture, alternate history, and neo-noir.",
-        movie: ""
-    },
-    {
-        name: "Sergio Leone",
-        birthYear: "January 3, 1929",
-        bio: "Sergio Leone was an Italian film director, producer and screenwriter, credited as the creator of the Spaghetti Western genre and widely regarded as one of the most influential directors in the history of cinema.",
-        movie: ""
-    }
-];
-
-let users = [ 
-    {
-        username: "keefe",
-        password: "adshjk",
-        movieList: "thor",
-        id: "1"
-    },
-    {
-        username: "jin",
-        password: "kazama",
-        movieList: "tekken",
-        id: "2"
-    },
-    {
-        username: "sosuke",
-        password: "aizen",
-        movieList: "bleach",
-        id: "3"
-    }
-];
-
 //Return a greeting message to test server
 app.get('/', (req, res) => {
   res.send('Welcome to Movie REST API!');
@@ -206,18 +76,26 @@ app.get('/', (req, res) => {
 
 //Return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
-  res.json(movies);
+    Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 //Return a single movie by title to the user
 app.get('/movies/:title', (req, res) => {
-    const _title = req.params.title
-    movies.forEach((movie) => {
-        if (movie.title === capitalizeTheFirstLetterOfEachWord(_title)) {
-            res.json(movie);
-        } 
+    Movies.findOne( {Title : req.params.Title} )
+    .then((title) => {
+        res.status(201).json(title);
     })
-    return res.status(404).send("Movie not found.");
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 //return a single genre by name to user
