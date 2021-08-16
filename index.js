bf526d6a3e22aec0d5b3d6cd51603bcaf817fbf2
keefeharris <keefeharrisjr@gmail.com>
2021-08-16 19:45:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const app = express();
-//the module for express is called
-//the module express is encapsulated 
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+const passport = require('passport');
+require('./passport');
 
 const mongoose = require('mongoose');
 const Models = require('./models.js');
@@ -11,47 +12,23 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
+const app = express();
+//the module for express is called
+//the module express is encapsulated 
+
+const { check, validationResult } = require('express-validator');
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.json());
+let auth = require('./auth')(app);
+//Please note the app argument you're passing here. This ensures that Express is available in your “auth.js” file as well.
+
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {
   useNewUrlParser: true, useUnifiedTopology: true
 });
 //allows mongoose to connect to database to perform CRUD operations
-
-
-const bodyParser = require('body-parser');
-//the module for body-parser is called
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const morgan = require('morgan');
-//the module for morgan is called
-const uuid = require('uuid');
-//the module for uuid is called
-
-let myLogger = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
-//this middle ware logs the url in the terminal everytime a request is made
-
-let requestTime = (req, res, next) => {
-  req.requestTime = Date.now();
-  next();
-};
-//this is a middleware function that logs a time upon req (timestamp)
-
-app.use(myLogger);
-app.use(requestTime);
-//invoking the app.use on the middleware function ensures that these middleware functions will be called upon every request
-app.use(morgan('common'));
-/*
-Morgan’s “common” format, which logs basic data such as IP address, the time of the request, 
-the request method and path, as well as the status code that was sent back as a response.
-"check terminal for date, time, etc....." 
-*/
-
-
-
-
 
 //Return a greeting message to test server
 app.get('/', (req, res) => {
@@ -59,16 +36,16 @@ app.get('/', (req, res) => {
 });
 
 //Return a list of ALL movies to the user
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
-    .then((movies) => {
+      .then((movies) => {
         res.status(201).json(movies);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-    });
-});
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 //Return a list of all users
 app.get('/users', (req, res) => {
@@ -82,11 +59,10 @@ app.get('/users', (req, res) => {
 });
 
 //Return a single movie by title to the user
-app.get("/Movies/:Title", (req, res) => {
-    const mtitle = req.params.Title;
-    Movies.findOne({Title : mtitle})
-    .then((movie_t) => {
-        res.json(movie_t);
+app.get('/movies/:title', (req, res) => {
+    Movies.findOne( { Title : req.params.title } )
+    .then((movie) => {
+        res.json(movie);
     })
     .catch((err) => {
         console.error(err);
@@ -95,7 +71,7 @@ app.get("/Movies/:Title", (req, res) => {
 });
 
 //return a single genre by name to user
-app.get('Movies/:Genre.Name', (req, res) => {
+app.get('/Movies/:Genre.Name', (req, res) => {
     Movie.findOne({Name : req.params.Name})
     .then((genre_name) => {
         res.json(genre_name);
@@ -104,27 +80,6 @@ app.get('Movies/:Genre.Name', (req, res) => {
         res.status(500).send('Error: ' + err);
     });
 });
-/* ----old request
-/*
-function capitalizeTheFirstLetterOfEachWord(words) {
-    var separateWord = words.toLowerCase().split(' ');
-    for (var i = 0; i < separateWord.length; i++) {
-       separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
-       separateWord[i].substring(1);
-    }
-    return separateWord.join(' ');
-}
-
-app.get('/genres/:name', (req, res) => {
-    const _gname = req.params.name
-    genres.forEach((genre_name) => {
-        if (genre_name.name === capitalizeTheFirstLetterOfEachWord(_gname)) {
-            res.json(genre_name);
-        }
-    })
-    return res.status(404).send("Genre not found.");
-});
-*/
 
 //Return a single director by name to user
 app.get('/Movies/:Director.Name', (req,res) => {
@@ -137,20 +92,21 @@ app.get('/Movies/:Director.Name', (req,res) => {
     });
 });
 
-/* -----old request
-app.get('/directors/:name', (req, res) => {
-    const _name = req.params.name
-    directors.forEach((director) => {
-        if (director.name === capitalizeTheFirstLetterOfEachWord(_name)) {
-            res.json(director);
-        }
-    })
-    return res.status(404).send("Director not found.");
-});
-*/
-
 //Allow new users to register
-app.post('/users', (req, res) => {
+app.post('/users', [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+       // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    
+    let hashedPassword = User.hashPassword(req.body.password);
     Users.findOne( {Username: req.body.Username} )
     .then((user) => {
         if (user) {
@@ -158,7 +114,7 @@ app.post('/users', (req, res) => {
         } else {
           Users.create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            password: hashedPassword,
             Email: req.body.Email,
             Birthday:  req.body.Birthday,
             Favorites: req.body.Favorites
@@ -175,25 +131,6 @@ app.post('/users', (req, res) => {
         res.status(500).send('Error: ' + error);
     });
 });
-/* ----old request
-app.post('/users', (req, res) => {
-    let newUser = req.body;
-
-    if (!newUser.name) {
-        const message = 'The username is missing.';
-        res.status(400).send(message);
-    } else {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).send(newUser);
-    }
-});
-*/
-
-
-
-
-
 
 //Allow users to update information
 app.put('/Users/:Username', (req, res) => {
@@ -217,20 +154,7 @@ app.put('/Users/:Username', (req, res) => {
         }
     });
 });
-/* ----old request
-app.put('/users/:username', (req, res) => {
-    let user = users.find((p_user) => {
-        return p_user.username === req.params.username
-    });
 
-    if (user) {
-        user.username === req.params.username;
-        res.status(201).send('User ' + req.params.username + ' will be assigned a new username.');
-    } else {
-        res.status(404).send('Username' + req.params.username + 'was not found');
-    };
-});
-*/
 
 //Allow users to add a movie to the list
 app.post('/Users/:Username/Movies/:_id', (req, res) => {
@@ -246,20 +170,6 @@ app.post('/Users/:Username/Movies/:_id', (req, res) => {
             }
     });
 });
-/*
-app.post('/users/:movieList', (req, res) => {
-    let newMovie = req.body;
-
-    if (!newMovie.movieList) {
-        const message = 'Unable to add movie to list.';
-        res.status(400).send(message);
-    } else {
-        newMovie.id = uuid.v4();
-        users.push(newMovie);
-        res.status(201).send(newMovie);
-    }
-});
-*/
 
 //Allow user to remove a movie from the list
 app.delete('/Users/:Username/Movies/:_id', (req, res) => {
@@ -277,11 +187,6 @@ app.delete('/Users/:Username/Movies/:_id', (req, res) => {
         }
     );
 });
-
-
-
-
-
 
 //Allow user to deregister
 app.delete('/Users/:Username', (req, res) => {
