20759168f8e2f6e0b9483f0d6adff34d8d01019c
Keefe Harris <keefeharrisjr@gmail.com>
2021-10-14 20:37:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ const app = express();
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:1234'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 /*
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {
